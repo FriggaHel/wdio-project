cb6485a6a910512af8c77cbcf2fd5bd3c61f4652
Felix P <felix@saucelabs.com>
2024-09-02 12:48:09 +0200

    Integration Trial

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,15 +1,50 @@
+import { Eyes, 
+    ClassicRunner,
+    Target, 
+    RectangleSize, 
+    Configuration, 
+    BatchInfo,
+ } from '@applitools/eyes-webdriverio';
+
 import LoginPage from '../pageobjects/login.page'
 import InventoryPage from '../pageobjects/inventory.page'
 
 describe('Sauce Demo', () => {
+    // Credentials
+    let applitoolsApiKey;
+
+    // Applitools objects to share for all tests
+    let batch;
+    let config: Configuration;
+    let runner: ClassicRunner;
+  
+    // Test-specific objects
+    let eyes;
+
     before(async function () {
+        applitoolsApiKey = process.env.APPLITOOLS_API_KEY!;
+        runner = runner = new ClassicRunner();
+        batch = new BatchInfo(`Sauce Demo with Classical Runner`);
+        config = new Configuration();
+        config.setApiKey(applitoolsApiKey);
+        config.setBatch(batch);
     });
 
     beforeEach(async function () {
+        eyes = new Eyes(runner);
+        eyes.setConfiguration(config);
+
+        browser = await eyes.open(
+            browser,
+            'Sauce Demo',
+            this.currentTest!.fullTitle(),
+            new RectangleSize(1200, 600)
+          );
     });
   
   
     afterEach(async () => {
+        await eyes!.closeAsync();
     });
       
     after(async () => {
@@ -17,12 +52,12 @@ describe('Sauce Demo', () => {
 
     it('should login with valid credentials', async () => {
         await LoginPage.open();
-        // Capture screenshot
+        await eyes!.check(Target.window().fully().withName("Login page"));
 
         await LoginPage.login('standard_user', 'secret_sauce');
 
         await InventoryPage.fourthImage().waitForExist();
-        // Capture screenshot
+        await eyes!.check(Target.window().fully().withName("Inventory page").layout());
     })
 
 
